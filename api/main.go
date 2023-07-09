7ae31b05008db255b30e3a9b52e2ac6464667e0d
mikepepping <mikepepping@gmail.com>
2023-07-09 16:54:36 +1000

    WIP -stripping out propel auth and using file system as storage. Goal is to make auth service with zero non OS dependencies

--- api/main.go
+++ api/main.go
@@ -2,38 +2,78 @@ package main
 
 import (
 	"os"
+  "fmt"
+  "path/filepath"
 	"encoding/json"
 	"net/http"
 	"github.com/gorilla/mux"
-	propelauth "github.com/propelauth/propelauth-go/pkg"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/me", MeHandler)
+	router.HandleFunc("/me", asContentType("application/json", MeHandler))
 	http.ListenAndServe(":8080", router)
 }
 
+
+// BEGIN DataStore
+type DataStore struct {
+  RootDir string
+  FileName string
+}
+
+func (ds DataStore) InitDataStore() {
+  _, err := os.Stat(ds.RootDir)
+  if (os.IsNotExist(err)) {
+    fmt.Println("ERROR - data store directory does not exist")
+    panic(err)
+  }
+}
+
+
+func (ds DataStore) Store(dataType string, id string, data any) {
+  filename := id + ".json"
+  fullFilepath := filepath.Join(ds.RootDir, dataType, filename)
+  file, err := os.Create(fullFilepath)
+  if (err != nil) {
+    fmt.Printf("ERROR - Could not store %s with id %s in file %s", dataType, id, fullFilepath)
+    panic(err)
+  }
+
+  file.Close()
+}
+
+// END DataStore
+
+type UserRow struct {
+  Email string
+  GivenName string
+  LastName string
+  Password string
+}
+
+func (ur UserRow) Id() string {
+  return ur.Email
+}
+
+func (ur UserRow) DataType() string {
+  return "user"
+}
+
+
+// "middleware" for setting a routes content type
+func asContentType(contentType string, next http.HandlerFunc) http.HandlerFunc {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", contentType)
+
+    next.ServeHTTP(w, r)
+  })
+}
+
 func MeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-
-	propelApiKey := os.Getenv("PROPELAUTH_API_KEY")
-	propelAuthUrl := os.Getenv("PROPELAUTH_AUTH_URL")
-	propel_client, err := propelauth.InitBaseAuth(propelAuthUrl, propelApiKey, nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	user, err := propel_client.GetUser(r.Header.Get("Authorization"))
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
 	w.WriteHeader(http.StatusOK)
 
-	serialized, err	:= json.Marshal(user)
+	serialized, err	:= json.Marshal(struct{}{})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
